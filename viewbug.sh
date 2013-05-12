8a3c42c3a98d8a0f6fb40a47ee396c2475127519
Jens John <jens.o.john@gmail.com>
2013-05-12 18:04:31 +0200

    added license and usage information

--- viewbug.sh
+++ viewbug.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# viewbug.sh - CLI reader for Debian bug reports
+# viewbug.sh - CLI for reading Debian bug reports
 # Copyright (C) 2013 Jens Oliver John <asterisk@2ion.de>
 # 
 # This program is free software: you can redistribute it and/or modify
@@ -27,10 +27,63 @@ F_OUT_COMBINE=0
 
 declare -a NLIST
 declare -a PLIST
+declare -a XARGV
 
 help () {
-    echo "$0 [-kxcovh] -p PKG -n BUG#"
-    echo "Development homepage: https://github.com/2ion/viewbug"
+    echo '
+    viewbugs.sh - Copyright (C) 2013 Jens Oliver John
+    This program comes with ABSOLUTELY NO WARRANTY. This is free
+    software, and you are welcome to redistribute it under certain
+    conditions. Refer to the LICENSE file in the distribution for
+    details.
+
+    viewbugs.sh is a CLI for reading Debian bug reports.
+
+    Usage:
+
+        viewbugs.sh [OPTIONS] <BUGLIST>
+
+        <BUGLIST> is a list of instances of the two options
+
+            -n BUG#
+            -p PKG[/version]
+
+        which refer to a bug by number or to all bugs of a package
+        respectively. BUG# has to be an integer. PKG is a package
+        name, optionally refering to a specific package version.
+
+    Options:
+
+        -A ARG
+            Add an extra argument ARG when calling apt-listbugs.
+            May be specified multiple times.
+            Refer to apt-listbugs(1) for more information.
+
+        -k
+            Keep the MBOX files with the retrieved bug reports.
+
+        -x COMMAND
+            Use COMMAND as the MBOX reader. $FILE in COMMAND will
+            be replaced with the path of the file to be displayed.
+            Defaults to "mutt -f $FILE".
+
+        -o NAME-TEMPLATE
+            Set the template used for the generation of the output
+            MBOX files. If $BUGNO or $G_FILEKEY occur, they will
+            expand to the current bug number and an integer, the
+            value of which is unique for each instance.
+            NAME-TEMPLATE **MUST** contain at least one instance of
+            $G_FILEKEY, because it is used for globbing.
+            Defaults to "$BUGNO-$G_FILEKEY.mbox".
+
+        -c
+            Combine all retrieved bug reports into a single MBOX file.
+        
+       -h   
+            Print this short synopsis.
+       -v  
+            Print the version.
+'
 }
 
 while getopts "ctx:o:vhn:p:" OPT; do
@@ -52,6 +105,7 @@ while getopts "ctx:o:vhn:p:" OPT; do
         n) NLIST=(${NLIST[@]} "$OPTARG") ;;
         p) PLIST=(${PLIST[@]} "$OPTARG") ;;
         c) F_OUT_COMBINE=1 ;;
+        A) XARGV=(${XARGV[@]} "$OPTARG") ;;
     esac
 done
 
@@ -61,7 +115,7 @@ for n in ${NLIST[@]}; do
 done
 
 for p in ${PLIST[@]}; do
-    for n in $(apt-listbugs -q list "$p" | grep -o '#[[:digit:]]\+' | tr -d '#'); do
+    for n in $(apt-listbugs -q ${XARGV[@]} list "$p" | grep -o '#[[:digit:]]\+' | tr -d '#'); do
         BUGNO=$n
         wget -q -O "$(eval echo $F_OUT_NAME)" "http://bugs.debian.org/cgi-bin/bugreport.cgi?mbox=yes;bug=$n"
     done
