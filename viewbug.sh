f40d8f9b719aaa7551b9c0296fccdb41b8ce9993
Jens John <jens.o.john@gmail.com>
2013-05-12 18:07:15 +0200

    bump version number to 0.1

--- viewbug.sh
+++ viewbug.sh
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-G_VERSION=0.1-rc
+G_VERSION=0.1
 G_FILEKEY=$$
 G_TMP=$(mktemp)
 
