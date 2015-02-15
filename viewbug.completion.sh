f62af997f866023758f02f080d4a37d4e5fa938e
Jens John <dev@2ion.de>
2015-02-15 11:28:23 +0100

    remove disfunct completion script ; bump version to 5

--- viewbug.completion.sh
+++
@@ -1,9 +0,0 @@
-_viewbug(){
-    local cur=${COMP_WORDS[COMP_CWORD]}
-    local prev=${COMP_WORDS[COMP_CWORD-1]}
-    if [[ ${#COMP_WORDS[@]} -eq 1 || $cur =~ -* ]]; then
-        COMP_REPLY=( $(compgen -W '-A -c -h -k -n -o -p -v -x' -- $cur) )
-        return
-    fi
-}
-complete -F _viewbug viewbug
